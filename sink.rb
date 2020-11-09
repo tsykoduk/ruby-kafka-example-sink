dbbe19a6c01e42e387f93d09705c2a2c7ca67e09
Greg Nokes <tsykoduk@gmail.com>
2020-11-09 10:53:50 -0800

    add kafkaka for troubleshooting

--- sink.rb
+++ sink.rb
@@ -3,6 +3,8 @@
 ENV['RAILS_ENV'] = ARGV.first || ENV['RAILS_ENV'] || 'development'
 require File.expand_path(File.dirname(__FILE__) + "/config/environment")
 
+#kafkakat?
+
 
 def process_messages
   i = o
