7826797102229ee212e8c1c8de35abc721139dad
Greg Nokes <tsykoduk@gmail.com>
2020-11-09 14:40:44 -0800

    shoots self in foot

--- app/consumers/account_sink_consumer.rb
+++ app/consumers/account_sink_consumer.rb
@@ -4,4 +4,9 @@ class AccountSinkConsumer < Racecar::Consumer
   def process(message)
     puts "Received message: #{message.value}"
   end
+  
+  
+rescue JSON::ParserError => e
+  puts "Failed to process message in #{message.topic}/#{message.partition} at offset #{message.offset}: #{e}"
+  # It's probably a good idea to report the exception to an exception tracker service.
 end
