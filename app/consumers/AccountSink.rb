5b7a89cfc74f8b14b7d8499060f33c4a3b99d4d3
Greg Nokes <tsykoduk@gmail.com>
2020-11-09 12:33:04 -0800

    shoots self in foot

--- app/consumers/AccountSink.rb
+++ app/consumers/AccountSink.rb
@@ -1,6 +1,10 @@
 class AccountSink < Racecar::Consumer
   subscribes_to "brave_connector_67887.salesforce.account", start_from_beginning: false
 
+  def initialize
+    puts "booting subscriber -- this is just here so I can make sure that the model is starting"
+  end
+
   def process(message)
     puts "Processing message: #{message.value}"
     data = JSON.parse(message.value)
